3baa40bf00de3cf9ff0464ec2eb4efb577eaf3b3
Yan_Gabriel <yangabriel2012@gmail.com>
2025-07-15 22:36:41 -0400

    feat: adicionando lista de icons

---
+++ src/assets/icons.tsx
@@ -0,0 +1,48 @@
+export const playIcon = (
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
+    <path d="M5 3L19 12L5 21V3Z" fill="currentColor" />
+  </svg>
+);
+
+export const pauseIcon = (
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
+    <rect x="6" y="4" width="4" height="16" fill="currentColor" />
+    <rect x="14" y="4" width="4" height="16" fill="currentColor" />
+  </svg>
+);
+
+export const resetIcon = (
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
+    <path
+      d="M12 5V2L8 6L12 10V7C15.31 7 18 9.69 18 13C18 16.31 15.31 19 12 19C8.69 19 6 16.31 6 13H4C4 17.42 7.58 21 12 21C16.42 21 20 17.42 20 13C20 8.58 16.42 5 12 5Z"
+      fill="currentColor"
+    />
+  </svg>
+);
+
+export const notFoundTrajectoryIcon = (
+  <svg width="48" height="48" viewBox="0 0 24 24" fill="none">
+    <path
+      d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM12 20C7.59 20 4 16.41 4 12C4 7.59 7.59 4 12 4C16.41 4 20 7.59 20 12C20 16.41 16.41 20 12 20ZM11 16H13V18H11V16ZM12 6C9.79 6 8 7.79 8 10H10C10 8.9 10.9 8 12 8C13.1 8 14 8.9 14 10C14 12 11 11.75 11 15H13C13 12.75 16 12.5 16 10C16 7.79 14.21 6 12 6Z"
+      fill="currentColor"
+    />
+  </svg>
+);
+
+export const speedIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    className="lucide lucide-chevrons-up-icon lucide-chevrons-up"
+  >
+    <path d="m17 11-5-5-5 5" />
+    <path d="m17 18-5-5-5 5" />
+  </svg>
+);
