f64e0092c148efb439627d065acd025b6612241e
Yan_Gabriel <yangabriel2012@gmail.com>
2025-07-15 23:59:57 -0400

    feat: adicionando o title da tela

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,20 @@ import styles from "./app.module.scss";
 import { Header } from "./components/Header";
 import { CarAnimator } from "./components/CarAnimator";
 import { useTranslation } from "react-i18next";
+import { Footer } from "./components/Footer";
 
 const App: React.FC = () => {
   const { t } = useTranslation();
   return (
     <div className={styles.appContainer}>
       <Header />
+
       <main className={styles.mainContent}>
+        <div className={styles.pageTitle}>
+          <h1>
+            Monitoramento <span>de rota:</span>
+          </h1>
+        </div>
         {gpsData.courses && gpsData.courses.length > 0 ? (
           <>
             <CarAnimator trajectory={gpsData.courses[0].gps} />
@@ -19,7 +26,7 @@ const App: React.FC = () => {
           <p className={styles.noDataMessage}>{t("notFoundTrajectory")}</p>
         )}
       </main>
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 };
